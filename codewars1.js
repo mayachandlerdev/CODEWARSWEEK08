edd5a6340f6265cc71e493d626b53f9de909531f
Maya Chandler <55395525+mayachandlerdev@users.noreply.github.com>
2019-10-29 09:37:17 -0400

    Add files via upload

---
+++ codewars1.js
@@ -0,0 +1,15 @@
+//Messi goals function
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+function goals (num1, num2, num3) {
+ let laLigaGoals = num1
+ let copaDelReyGoals = num2
+ let championsLeagueGoals = num3
+ let sum = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  return sum
+}
